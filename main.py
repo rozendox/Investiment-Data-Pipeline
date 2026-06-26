fb5ccaf4a2490c93a556cd4e78b5cb05986680c6
Gabriel-Rozendo <gabrielsg236@gmail.com>
2026-06-26 17:34:08 -0400

    feat: implementa o esqueleto inicial da funcao orquestradora dos fluxos das pipelines.

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Modulo responsavel por gerir o fluxo da pipeline"""
+
+# --------------------------------------
+# area de instanciacao de pipelines.
+#--------------------------------------
+
+
+
+# --------------------------------------
+# func princpal
+# --------------------------------------
+def main():
+    ...
